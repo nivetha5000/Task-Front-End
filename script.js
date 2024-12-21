a3446e933cda4e31963ef0d20253d070f9e9e329
nivetha5000 <144010485+nivetha5000@users.noreply.github.com>
2024-12-21 13:46:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function playVideo() {
+    const videoContainer = document.getElementById('video-container');
+    const video = document.getElementById('video');
+    
+   
+    videoContainer.style.display = 'block';
+    video.play();
+  }
+  
